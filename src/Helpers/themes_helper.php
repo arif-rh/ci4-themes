c3824f26972fc4244b12cad963fc1a3d815328a1
Arif RH <arifrahmanhakim.net@gmail.com>
2020-04-04 22:30:00 +0700

    update helper codeCoverageIgnore

--- src/Helpers/themes_helper.php
+++ src/Helpers/themes_helper.php
@@ -1,35 +1,43 @@
 <?php
 
+// @codeCoverageIgnoreStart
 if (! function_exists('link_tag'))
 {
+// @codeCoverageIgnoreEnd
 	// generate link css tags
 	function link_tag($css)
 	{
 		return '<link rel="stylesheet" href="' . $css . '" />' . PHP_EOL;
 	}
+// @codeCoverageIgnoreStart
 }
 
 if (! function_exists('script_tag'))
 {
+// @codeCoverageIgnoreEnd
 	// generate js script tags
 	function script_tag($js)
 	{
 		return '<script src="' . $js . '"></script>' . PHP_EOL;
 	}
+// @codeCoverageIgnoreStart
 }
 
 if (! function_exists('validate_ext'))
 {
+// @codeCoverageIgnoreEnd
 	// validate file to have required exyension
 	function validate_ext($file, $ext = '.php')
 	{
 		$fileExt  = pathinfo($file, PATHINFO_EXTENSION);
 		return empty($fileExt) ? $file . $ext : $file; 
 	}
+// @codeCoverageIgnoreStart
 }
 
 if (! function_exists('theme_url'))
 {
+// @codeCoverageIgnoreEnd
 	// return full path from active theme URL
 	function theme_url($path = null)
 	{
@@ -37,10 +45,12 @@ if (! function_exists('theme_url'))
 
 		return $themeVars['theme_url'] . (is_string($path) ? $path : '');
 	}
+// @codeCoverageIgnoreStart
 }
 
 if (! function_exists('image_url'))
 {
+// @codeCoverageIgnoreEnd
 	// return full path to image URL in active theme
 	function image_url($path = null)
 	{
@@ -48,10 +58,12 @@ if (! function_exists('image_url'))
 
 		return $themeVars['image_url'] . (is_string($path) ? $path : '');
 	}
+// @codeCoverageIgnoreStart
 }
 
 if (! function_exists('plugin_url'))
 {
+// @codeCoverageIgnoreEnd
 	// return full path to plugin URL in active theme
 	function plugin_url($path = null)
 	{
@@ -59,4 +71,6 @@ if (! function_exists('plugin_url'))
 
 		return $themeVars['plugin_url'] . (is_string($path) ? $path : '');
 	}
+// @codeCoverageIgnoreStart
 }
+// @codeCoverageIgnoreEnd
